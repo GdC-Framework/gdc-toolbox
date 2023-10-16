113ad1e88a97e99b8962e5b81ce8aea654ccfd92
zalexki <a.haumaitre@cityscoot.eu>
2023-10-16 17:20:19 +0200

    use lib icons

--- components/DarkMode.vue
+++ components/DarkMode.vue
@@ -1,13 +1,11 @@
 <template>
     <div style="display: flex; align-items: center; margin-left: auto; margin-left:1rem;">
         <InputSwitch v-model="isDark" />
-        <img :src="isDark ? darkicon : lighticon" style="margin-left:0.5rem;" width="30"/> 
+        <i :class="isDark ? 'pi pi-moon' : 'pi pi-sun'" style="margin-left:0.5rem;font-size:2rem;" :style="isDark ? 'color:#F6F1D5' : 'color:#ebc834'" /> 
     </div>
 </template>
 
 <script setup>
-import darkicon from '@/assets/img/dark-theme.svg'
-import lighticon from '@/assets/img/light-theme.svg'
 import InputSwitch from 'primevue/inputswitch'
 import {useDark} from '@vueuse/core'
 const isDark = useDark();
