74a380238157d72af78e96d87d06a6aa1c8f1c4a
oxypomme <oxytom8@gmail.com>
2024-02-04 18:19:30 +0100

    fix(frontend): :arrow_down: downgraded primevue to a more stable version

--- components/DarkMode.vue
+++ components/DarkMode.vue
@@ -1,12 +1,13 @@
 <template>
     <div style="display: flex; align-items: center; margin-left: auto; margin-left:1rem;">
         <InputSwitch v-model="isDark" />
-        <i :class="isDark ? 'pi pi-moon' : 'pi pi-sun'" style="margin-left:0.5rem;font-size:1.5rem;" :style="isDark ? 'color:#F6F1D5' : 'color:#ebc834'" /> 
+        <i :class="isDark ? 'pi pi-moon' : 'pi pi-sun'" style="margin-left:0.5rem;font-size:1.5rem;" :style="isDark ? 'color:#F6F1D5' : 'color:#ebc834'" />
     </div>
 </template>
 
 <script setup>
 import InputSwitch from 'primevue/inputswitch'
-import {useDark} from '@vueuse/core'
+import { useDark } from '@vueuse/core'
+
 const isDark = useDark();
 </script>
